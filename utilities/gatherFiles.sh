a6b1a4ab7367532b45a992754c3c3ed165b4181b
Thomas Mansencal <thomas.mansencal@gmail.com>
2014-04-05 08:31:08 +0200

    Update "utilities/gatherFiles.sh" file.

--- utilities/gatherFiles.sh
+++ utilities/gatherFiles.sh
@@ -3,21 +3,21 @@ echo ----------------------------------------------------------------
 echo sIBL_GUI For 3dsMax - Files Gathering
 echo ----------------------------------------------------------------
 
-export PROJECT=/Users/KelSolaar/Documents/Development/sIBL_GUI_For_3dsMax
+export PROJECT_DIRECTORY=$(cd $( dirname "${BASH_SOURCE[0]}" )/..; pwd)
 
-export SOURCE=$PROJECT/src
-export RELEASES=$PROJECT/releases
-export REPOSITORY=$RELEASES/repository
+export SOURCE_DIRECTORY=$PROJECT_DIRECTORY/src
+export RELEASES_DIRECTORY=$PROJECT_DIRECTORY/releases
+export REPOSITORY_DIRECTORY=$RELEASES_DIRECTORY/repository
 
 #! Gathering folder cleanup.
-rm -rf $REPOSITORY/*
+rm -rf $REPOSITORY_DIRECTORY/*
 
 #! Change Log gathering.
-cp $RELEASES/Change_Log.html $REPOSITORY/
+cp $RELEASES_DIRECTORY/Change_Log.html $REPOSITORY_DIRECTORY/
 
 #! Helper script gathering.
-cd $SOURCE
-zip -r $REPOSITORY/sIBL_GUI_For_3dsMax.zip *.ms ../README
+cd $SOURCE_DIRECTORY
+zip -r $REPOSITORY_DIRECTORY/sIBL_GUI_For_3dsMax.zip *.ms ../README
 
 #! Reaching original directory.
 cd ../
